e915500121b1449adc1d7aedddf18c6f0c708261
Miren Chapaneri <280008137+mirenchaps@users.noreply.github.com>
2026-08-19 21:48:06 +0100

    adding helm charts

--- variables.tf
+++ variables.tf
@@ -38,7 +38,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version for the control plane (MAJOR.MINOR)."
   type        = string
-  default     = "1.33"
+  default     = "1.35"
 }
 
 # --- Networking ---
